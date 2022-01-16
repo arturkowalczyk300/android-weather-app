e573dfe4fdb8d3b88ccc130e1f377930a917db06
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-01-16 13:30:45 +0100

    fix not working validator for City input field in AddEditWeatherReading activity

--- app/src/main/java/com/arturkowalczyk300/mvvmlivedataexampleproject/View/AddEditWeatherReading.java
+++ app/src/main/java/com/arturkowalczyk300/mvvmlivedataexampleproject/View/AddEditWeatherReading.java
@@ -222,6 +222,23 @@ public class AddEditWeatherReading extends AppCompatActivity {
                 validator.validate(false);
             }
         });
+
+        editTextCity.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                validator.validate(false);
+            }
+        });
     }
 
     private void saveWeatherReading() {
